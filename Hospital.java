2ef219927f3d52813fbb29c4ec12cc14f3b12113
Nguyen Minh Khoi <khoinm5789@ut.edu.vn>
2026-05-19 20:42:31 +0700

    DOC-46
    git commit -m "DOC-46 Hospital"

---
+++ Hospital.java
@@ -0,0 +1,9 @@
+package com.dcdoctor.model;
+
+public class Hospital extends User {
+
+    public Hospital(int id, String name, String email, String role) {
+        super(id, name, email, role);
+    }
+
+}
